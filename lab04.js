746308bd64564388dce59e101ef248735c8f815e
mahmoud-salameh <mahmoud4561@hotmail.com>
2021-04-01 15:00:25 +0300

    the new work on js

---
+++ lab04.js
@@ -0,0 +1,18 @@
+var tempreature = prompt("What's the tempreature today?");
+            
+
+var temp  = ''
+
+if (tempreature > 20) {temp = " <p>it is a nice day</p>"}
+else if (tempreature < 20) {  temp = " <p>it is a cold day</p>" } 
+else {  sti = " <p>have a good DAY</p>" }
+
+document.write(temp);
+
+
+var team = "FC Barcelona the best team in the world"
+            
+
+
+document.write(team);
+
