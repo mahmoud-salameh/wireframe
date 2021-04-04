99bec075626fcf0899927996bd388347a11b91ed
mahmoud-salameh <mahmoud4561@hotmail.com>
2021-04-04 14:21:34 +0300

    readditing the js lab04

--- lab04.js
+++ lab04.js
@@ -10,7 +10,44 @@ else {  sti = " <p>have a good DAY</p>" }
 document.write(temp);
 
 
-var team = "FC Barcelona the best team in the world"
+
+
+var working = prompt("how many day you work a weak?");
+            
+
+var day  = ''
+
+if (working > 5) { day = " <p>you need to take a break</p>" }
+else if (working < 5) {  day = " <p>it is a good job if it is lees than 5 days</p>" } 
+else {  day = " <p>wish you luck</p>" }
+
+document.write(day);
+
+
+
+
+var family = prompt("how many kids you have?");
+            
+
+var kids  = ''
+
+if (family > 5) { kids = " <p>i hope the all in a good health</p>" }
+else if (family < 5) {  kids = " <p>it is good to have less than 5 kids i thank</p>" } 
+else {  kids = " <p>i wish the all become doktors</p>" }
+
+document.write(kids);
+
+
+
+
+
+
+
+
+
+
+
+var team = "FC Barcelona is the best team in the world"
             
 
 
